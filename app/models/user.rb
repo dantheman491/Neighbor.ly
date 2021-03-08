bef3af472a6a44471ee4bcf86d30c51342039a38
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-03-08 14:07:51 -0500

    seed data setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-has_many :listings
-has_many :reviews
-
+  has_many :listings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.destroy_all
+Review.destroy_all
+User.destroy_all
+
+@dan = User.create!(username: "dan", email: "dan@gmail.com", password: "1234567")
+
+puts "#{User.count} users created"
+
+listing1 = Listing.create!(img_url: 'https://images.pexels.com/photos/4491852/pexels-photo-4491852.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', item_title: 'circular saw', user: @dan)
+listing2 = Listing.create!(item_title: 'ladder', user: @dan)
+
+puts "#{Listing.count} listing created"
+
+review1 = Review.create!(content: 'this item is in great condition, thanks neighbor', user: @dan, listing_id: listing1)
+
+puts "#{Review.count} review created"
