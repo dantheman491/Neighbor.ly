273c41cb40aeaa07a91240ceca6ba4d1cc27d15f
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-03-11 10:56:18 -0500

    added assets

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
-
+import Githublogo from "../../../assets/Githublogo.png";
+import drlogo from "../../../assets/drlogo.png";
+import Linkedinlogo from "../../../assets/Linkedinlogo.png";
 const Footer = () => {
   return (
     <div className="footer">
       <Link to="/">
         <h1 className="footer-h1">Neighbor.ly</h1>
       </Link>
-      {/* <div className="three-logos">
+      <div className="three-logos">
         <div className="first-one">
-          <a href="https://github.com/dantheman491/Neighbor.ly">logo1</a>
+          <img src={Githublogo} />
+          <a href="https://github.com/dantheman491/Neighbor.ly"></a>
         </div>
-        <img src={require("../../assets/Githublogo.png")} />
         <div className="second-one">
-          <a href="https://github.com/dantheman491/Neighbor.ly">logo1</a>
+          <a href="https://www.linkedin.com/in/danielatilio/"></a>
+          <img src={Linkedinlogo} />
         </div>
         <div className="third-one">
-          <a href="https://github.com/dantheman491/Neighbor.ly">logo1</a>
+          <a href="https://www.danielramirez.design/"></a>
+          <img src={drlogo} />
         </div>
-      </div> */}
+      </div>
       <div className="footer-text">
         <h4 className="footer-h4">Daniel Ramirez 2021</h4>
         <div>
